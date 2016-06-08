1f1840be5bbf018c77006a2aef42bb4832e259c5
pnisarg <pnisarg26@yahoo.com>
2016-06-08 16:25:41 -0700

    section nesting

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,9 +2,11 @@ class SectionsController < ApplicationController
   layout "admin"
 
   before_action :confirm_logged_in
-  
+  before_action :find_page
+
   def index
-    @sections = Section.sorted
+    # @sections = Section.sorted
+    @sections = @page.sections.sorted
   end
 
   def edit
@@ -17,7 +19,7 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
     if @section.update_attributes(section_params)
       flash[:notice] = "Section updated successfully."
-      redirect_to(:action => 'show', :id => @section.id)
+      redirect_to(:action => 'show', :id => @section.id, :page_id => @page.id, :subject_id => @subject.id)
     else
       @pages = Page.order("position ASC")
       @section_count = Section.count
@@ -33,11 +35,11 @@ class SectionsController < ApplicationController
     section = Section.find(params[:id])
     section.destroy
     flash[:notice] = "Section '#{section.name}' destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :page_id => @page.id, :subject_id => @subject.id)
   end
 
   def new
-    @section = Section.new()
+    @section = Section.new({:page_id => @page.id})
     @pages = Page.order("position ASC")
     @section_count = Section.count
   end
@@ -46,7 +48,7 @@ class SectionsController < ApplicationController
     @section = Section.new(section_params)
     if @section.save
       flash[:notice] = "Section created successfully."
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :page_id => @page.id, :subject_id => @subject.id)
     else
       @pages = Page.order("position ASC")
       @section_count = Section.count
@@ -58,7 +60,14 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
   end
 
+  private
   def section_params
     params.require(:section).permit(:page_id, :name, :position, :content_type, :content, :visible)
+  end
+  def find_page
+    if params[:page_id]
+      @page = Page.find(params[:page_id])
+      @subject = Subject.find(params[:subject_id]) 
+    end
   end
 end
